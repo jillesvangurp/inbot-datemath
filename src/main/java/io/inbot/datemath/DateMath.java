95e5c3197c643f785f8f8bd8a4b6730b6c092b08
Jilles van Gurp <jilles@jillesvangurp.com>
2014-12-27 13:52:33 +0100

    initial commit

---
+++ src/main/java/io/inbot/datemath/DateMath.java
@@ -0,0 +1,213 @@
+package io.inbot.datemath;
+
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Utility class to assist with parsing iso timestamps or expressions that manipulate such timestamps to a
+ * java.time.Instant.
+ */
+public class DateMath {
+    private static final Pattern DURATION_PATTERN = Pattern.compile("-?\\s*([0-9]+)\\s*([s|h|d|w|m|y])");
+    private static final Pattern SUM_PATTERN = Pattern.compile("(.+)\\s*([\\+-])\\s*(.+)");
+
+    public static String formatIsoDateNow() {
+        return formatIsoDate(Instant.now());
+    }
+
+    public static String formatIsoDate(LocalDate date) {
+        LocalDateTime time = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), 0, 0, 0);
+        return formatIsoDate(time.toInstant(ZoneOffset.UTC));
+    }
+
+    public static String formatIsoDate(LocalDateTime time) {
+        return formatIsoDate(time.toInstant(ZoneOffset.UTC));
+    }
+
+    public static String formatIsoDate(OffsetDateTime date) {
+        return formatIsoDate(date.toInstant());
+    }
+
+    public static String formatIsoDate(Instant date) {
+        return date.truncatedTo(ChronoUnit.SECONDS).toString();
+    }
+
+    public static String formatIsoDate(long timeInMillisSinceEpoch) {
+        return formatIsoDate(Instant.ofEpochMilli(timeInMillisSinceEpoch));
+    }
+
+    private static Instant flexibleInstantParse(String text) throws DateTimeParseException {
+        try {
+            return Instant.parse(text);
+        } catch (DateTimeParseException e) {
+            // fallback to LocalDate and then pretend is at midnight
+            LocalDate parsed = LocalDate.parse(text);
+            return Instant.parse(parsed + "T00:00:00Z");
+        }
+    }
+
+    public static Instant parse(String text) {
+        if (text == null) {
+            throw new IllegalArgumentException("cannot parse empty string");
+        }
+        text = text.trim();
+        try {
+            return flexibleInstantParse(text);
+        } catch (DateTimeParseException e) {
+            return toInstant(parseRelativeTime(text, null));
+        }
+    }
+
+    private static LocalDateTime parseRelativeTime(String text, String zoneId) {
+        if(zoneId == null) {
+            zoneId="Z";
+        }
+        LocalDateTime now=LocalDateTime.ofInstant(Instant.now(), ZoneId.of(zoneId));
+        switch (text) {
+        case "now":
+            return now;
+        case "beginning_month":
+            return now.truncatedTo(ChronoUnit.DAYS).with(TemporalAdjusters.firstDayOfMonth());
+        case "end_month":
+            return now.truncatedTo(ChronoUnit.DAYS).with(TemporalAdjusters.firstDayOfNextMonth());
+        case "beginning_year":
+            return now.truncatedTo(ChronoUnit.DAYS).with(TemporalAdjusters.firstDayOfYear());
+        case "end_year":
+            return now.truncatedTo(ChronoUnit.DAYS).with(TemporalAdjusters.firstDayOfNextYear());
+        case "beginning_week":
+            return now.truncatedTo(ChronoUnit.DAYS).with(TemporalAdjusters.previous(DayOfWeek.SUNDAY));
+        case "end_week":
+            return now.truncatedTo(ChronoUnit.DAYS).with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+        case "tomorrow":
+            return now.truncatedTo(ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).plus(1, ChronoUnit.DAYS);
+        case "day_after_tomorrow":
+            return now.truncatedTo(ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).plus(2, ChronoUnit.DAYS);
+        case "yesterday":
+            return now.truncatedTo(ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).minus(1, ChronoUnit.DAYS);
+        case "day_before_yesterday":
+            return now.truncatedTo(ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).minus(2, ChronoUnit.DAYS);
+        case "next_month":
+            return parseRelativeTime("1m", zoneId);
+        case "last_month":
+            return parseRelativeTime("-1m",zoneId);
+        case "next_year":
+            return parseRelativeTime("1y",zoneId);
+        case "last_year":
+            return parseRelativeTime("-1y",zoneId);
+        default:
+            Matcher matcher = DURATION_PATTERN.matcher(text);
+            if (matcher.matches()) {
+                boolean minus = text.startsWith("-");
+                int amount = Integer.valueOf(matcher.group(1));
+                String unit = matcher.group(2);
+                ChronoUnit chronoUnit;
+                switch (unit) {
+                case "s":
+                    chronoUnit = ChronoUnit.SECONDS;
+                    break;
+                case "h":
+                    chronoUnit = ChronoUnit.HOURS;
+                    break;
+                case "d":
+                    chronoUnit = ChronoUnit.DAYS;
+                    break;
+                case "w":
+                    chronoUnit = ChronoUnit.DAYS;
+                    amount = amount * 7;
+                    break;
+                case "m":
+                    if (minus) {
+                        return now.minusMonths(amount);
+                    } else {
+                        return now.plusMonths(amount);
+                    }
+                case "y":
+                    if (minus) {
+                        return now.minusYears(amount);
+                    } else {
+                        return now.plusYears(amount);
+                    }
+                default:
+                    throw new IllegalArgumentException("illegal time unit. Should be [s|h|d|w|m|y]: " + text);
+                }
+                if (minus) {
+                    return now.minus(amount, chronoUnit);
+                } else {
+                    return now.plus(amount, chronoUnit);
+                }
+            } else {
+                matcher = SUM_PATTERN.matcher(text);
+                if(matcher.matches()) {
+                    String left = matcher.group(1);
+                    String operator = matcher.group(2);
+                    String right = matcher.group(3);
+                    Instant offset = parse(left);
+                    boolean minus = operator.equals("-");
+                    Matcher dm = DURATION_PATTERN.matcher(right);
+                    if(dm.matches()) {
+                        int amount = Integer.valueOf(dm.group(1));
+                        String unit = dm.group(2);
+                        ChronoUnit chronoUnit;
+                        switch (unit) {
+                        case "s":
+                            chronoUnit = ChronoUnit.SECONDS;
+                            break;
+                        case "h":
+                            chronoUnit = ChronoUnit.HOURS;
+                            break;
+                        case "d":
+                            chronoUnit = ChronoUnit.DAYS;
+                            break;
+                        case "w":
+                            chronoUnit = ChronoUnit.DAYS;
+                            amount = amount * 7;
+                            break;
+                        case "m":
+                            if (minus) {
+                                return LocalDateTime.ofInstant(offset, ZoneId.of("Z")).minusMonths(amount);
+                            } else {
+                                return LocalDateTime.ofInstant(offset, ZoneId.of("Z")).plusMonths(amount);
+                            }
+                        case "y":
+                            if (minus) {
+                                return LocalDateTime.ofInstant(offset, ZoneId.of("Z")).minusYears(amount);
+                            } else {
+                                return LocalDateTime.ofInstant(offset, ZoneId.of("Z")).plusYears(amount);
+                            }
+                        default:
+                            throw new IllegalArgumentException("illegal time unit. Should be [s|h|d|w|m|y]: " + text);
+                        }
+                        if (minus) {
+                            return LocalDateTime.ofInstant(offset, ZoneId.of("Z")).minus(amount, chronoUnit);
+                        } else {
+                            return LocalDateTime.ofInstant(offset, ZoneId.of("Z")).plus(amount, chronoUnit);
+                        }
+                    } else {
+                        throw new IllegalArgumentException("illegal duration. Should match ([0-9]+)([s|h|d|w|m|y]): " + right);
+                    }
+                }
+            }
+        }
+        throw new IllegalArgumentException("illegal timestamp " + text);
+
+    }
+
+    public static Instant toInstant(LocalDate date) {
+        return Instant.parse(date.toString() + "T00:00:00Z");
+    }
+
+    public static Instant toInstant(LocalDateTime dateTime) {
+        return dateTime.toInstant(ZoneOffset.UTC);
+    }
+
+}
---
+++ src/test/java/io/inbot/datemath/DateMathTest.java
@@ -0,0 +1,66 @@
+package io.inbot.datemath;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test
+public class DateMathTest {
+
+    public void shouldFormatLocalDate() {
+        assertThat(DateMath.formatIsoDate(LocalDate.of(1974, 10, 20))).isEqualTo("1974-10-20T00:00:00Z");
+    }
+
+    public void shouldFormatLocalDateTime() {
+        LocalDateTime time = LocalDateTime.of(1984, 12, 1, 0, 0, 0);
+        assertThat(DateMath.formatIsoDate(time)).isEqualTo("1984-12-01T00:00:00Z");
+    }
+
+    public void shouldParseIsoTimeStamp() {
+        assertThat(DateMath.parse("1974-10-20T00:00:00Z")).isEqualTo(Instant.parse("1974-10-20T00:00:00Z").truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    public void shouldSupportIsoDate() {
+        assertThat(DateMath.parse("1974-10-20")).isEqualTo(Instant.parse("1974-10-20T00:00:00Z").truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    @DataProvider
+    public Object[][] samples() {
+        LocalDateTime now = LocalDateTime.ofInstant(Instant.now(), ZoneId.of("Z"));
+        return new Object[][] {
+                {"tomorrow",now.truncatedTo(ChronoUnit.DAYS).plus(1, ChronoUnit.DAYS).toInstant(ZoneOffset.UTC)},
+                {"yesterday",now.truncatedTo(ChronoUnit.DAYS).minus(1, ChronoUnit.DAYS).toInstant(ZoneOffset.UTC)},
+                {"day_before_yesterday",now.truncatedTo(ChronoUnit.DAYS).minus(2, ChronoUnit.DAYS).toInstant(ZoneOffset.UTC)},
+                {"day_after_tomorrow",now.truncatedTo(ChronoUnit.DAYS).plus(2, ChronoUnit.DAYS).toInstant(ZoneOffset.UTC)},
+                {"-10s",Instant.now().minus(10, ChronoUnit.SECONDS)},
+                {"1d",Instant.now().plus(1, ChronoUnit.DAYS)},
+                {"-1d",Instant.now().minus(1, ChronoUnit.DAYS)},
+                {"-1w",Instant.now().minus(7, ChronoUnit.DAYS)},
+                {"-100m",now.minusMonths(100).toInstant(ZoneOffset.UTC)},
+                {"100m",now.plusMonths(100).toInstant(ZoneOffset.UTC)},
+                {"  -  100  y  ",now.minusYears(100).toInstant(ZoneOffset.UTC)},
+                {"100y",now.plusYears(100).toInstant(ZoneOffset.UTC)},
+                {"yesterday+100y",now.minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).plusYears(100).toInstant(ZoneOffset.UTC)},
+                {"yesterday-100y",now.minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).minusYears(100).toInstant(ZoneOffset.UTC)},
+                {"yesterday + 100y",now.minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).plusYears(100).toInstant(ZoneOffset.UTC)},
+                {"yesterday\t-\t100y",now.minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).minusYears(100).toInstant(ZoneOffset.UTC)}
+        };
+    }
+
+    @Test(dataProvider="samples")
+    public void shouldDoDateMath(String text, Instant expected) {
+        // allow for a few milliseconds difference between now during parsing and calculation of expected
+        assertThat(differenceInMillis(DateMath.parse(text), expected)).isLessThan(100);
+    }
+
+    private long differenceInMillis(Instant one, Instant two) {
+        return Math.abs(one.toEpochMilli()-two.toEpochMilli());
+    }
+}
